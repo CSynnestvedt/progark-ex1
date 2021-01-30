b3f01019acfed2a642ecb6361b9c54e954fc9817
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2021-01-30 21:40:48 +0100

    Add velocity and change in sprite

--- core/src/com/mygdx/game/sprites/PilotedHelicopter.java
+++ core/src/com/mygdx/game/sprites/PilotedHelicopter.java
@@ -12,7 +12,7 @@ public class PilotedHelicopter extends Helicopter {
     private static int TEXTURE_Y_CENTRE;
 
     Vector2 distanceRay = new Vector2();
-    Vector3 touch = new Vector3();
+    Vector2 touch = new Vector2();
     Camera cam;
 
     public PilotedHelicopter(int x, int y, Camera cam) {
@@ -25,30 +25,30 @@ public class PilotedHelicopter extends Helicopter {
     @Override
     public void update(float dt) {
         if (Gdx.input.isTouched()) {
-            touch.set(Gdx.input.getX(), Gdx.input.getY(), 0);
             // cam.unproject(touch); // Touch and position use different coordinate systems. Must unproject to match them.
-            handleMovement(dt, touch);
+            handleMovement(dt);
             updateTexture(); // In case copter is moving the other way now
         }
     }
 
-    private void handleMovement(float dt, Vector3 touch) {
+    private void handleMovement(float dt) {
+        touch.set(Gdx.input.getX(), Gdx.input.getY());
         float travelDistance = SPEED * dt;
-        float xSubtractionVal = position.x + TEXTURE_X_CENTRE;
         touch.y = Game.HEIGHT - touch.y;
-        distanceRay.set(touch.x, touch.y).sub(xSubtractionVal, position.y + TEXTURE_Y_CENTRE); // Vector from touchpoint to player
+        distanceRay.set(touch.x, touch.y).sub(position.x + TEXTURE_X_CENTRE, position.y + TEXTURE_Y_CENTRE); // Vector from touchpoint to player
         boolean canReachDest = distanceRay.len() <= travelDistance;
         if (canReachDest) {
             boolean touchToLeftOfCopter = touch.x < position.x;
             boolean touchAboveCopter = touch.y > position.y;
-            position.x = touchToLeftOfCopter ? touch.x + TEXTURE_X_CENTRE : touch.x - TEXTURE_X_CENTRE;
-            position.y = touchAboveCopter ? touch.y + TEXTURE_Y_CENTRE : touch.y - TEXTURE_Y_CENTRE;
+            velocity.x = touchToLeftOfCopter ? touch.x + TEXTURE_X_CENTRE : touch.x - TEXTURE_X_CENTRE;
+            velocity.y = touchAboveCopter ? touch.y + TEXTURE_Y_CENTRE : touch.y - TEXTURE_Y_CENTRE;
         } else {
             distanceRay.nor().scl(travelDistance);
             System.out.println("Scaled " + distanceRay);
-            position.x -= distanceRay.x;
-            position.y -= distanceRay.y;
+            velocity.x = -distanceRay.x;
+            velocity.y = -distanceRay.y;
         }
+        position.add(velocity);
     }
 
 }
